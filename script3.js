583c5ec7449c1cf8d1c280e0bb737febfa6c3fe5
Marc ROBERT <marc.robert@sam-assurance.com>
2018-10-09 13:19:41 +0200

    Ajout du script script3.js

---
+++ script3.js
@@ -0,0 +1,15 @@
+﻿
+function Highlight(e, className) {
+    if (className == null) {
+        className = "highlight";
+    }
+    overClassName = e.className;
+    e.className = className;
+}
+
+function Unhighlight(e, className) {
+    if (className == null) {
+        className = overClassName;
+    }
+    e.className = className;
+}
