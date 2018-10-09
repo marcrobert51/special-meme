5f4734ef630017e4ff42aff159a465a1ade6bed1
Marc ROBERT <marc.robert@sam-assurance.com>
2018-10-09 13:15:49 +0200

    Ajout des fichiers script1 et script2

---
+++ script1.js
@@ -0,0 +1,17 @@
+﻿
+//******************************************************************************************
+// Lecture d'un cookie 
+//******************************************************************************************
+
+function getCookie(szName) {
+    var szValue = null;
+    if (document.cookie)	   //only if exists
+    {
+        var arr = document.cookie.split(szName + '=');
+        if (2 <= arr.length) {
+            var arr2 = arr[1].split(';');
+            szValue = unescape(arr2[0]);
+        }
+    }
+    return szValue;
+}
---
+++ script2.js
@@ -0,0 +1,10 @@
+﻿//	Gestion du postback
+// save a reference to the original __doPostBack
+var __oldDoPostBack;
+function myPostBack() {
+    if (typeof __doPostBack != "undefined") {
+        window.__oldDoPostBack = window.__doPostBack;
+        // replace __doPostBack with another function
+        window.__doPostBack = window.AlwaysFireBeforeFormSubmit;
+    }
+}
